bfd661699831c0a573c0ce91abd2e44e444a128d
feng <frank@coloso.io>
2023-05-19 18:10:02 +0800

    380

--- 380.InsertDeleteGetRandom.go
+++ 380.InsertDeleteGetRandom.go
@@ -1,1 +1,49 @@
 package main
+
+import "math/rand"
+
+type RandomizedSet struct {
+	con  map[int]int
+	size int
+}
+
+func Constructor() RandomizedSet {
+	p := RandomizedSet{
+		con:  map[int]int{},
+		size: 0,
+	}
+	return p
+}
+
+func (this *RandomizedSet) Insert(val int) bool {
+	if _, ok := this.con[val]; ok {
+		return false
+	} else {
+		this.con[val] = 1
+		this.size++
+		return true
+	}
+}
+
+func (this *RandomizedSet) Remove(val int) bool {
+	if _, ok := this.con[val]; !ok {
+		return false
+	}
+	delete(this.con, val)
+	this.size--
+	return true
+}
+
+func (this *RandomizedSet) GetRandom() int {
+	if this.size == 0 {
+		return 0
+	}
+	n := rand.Intn(this.size)
+	keys := make([]int, this.size)
+	i := 0
+	for k := range this.con {
+		keys[i] = k
+		i++
+	}
+	return keys[n]
+}
--- 380.InsertDeleteGetRandom_test.go
+++ 380.InsertDeleteGetRandom_test.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_RandomizedSet(t *testing.T) {
+	p := Constructor()
+	assert.Equal(t, p.Insert(1), true, "1")
+	assert.Equal(t, p.Remove(2), false, "2")
+	assert.Equal(t, p.Insert(2), true, "3")
+	assert.Contains(t, []int{1, 2}, p.GetRandom(), "4")
+	assert.Equal(t, p.Remove(1), true, "4")
+	assert.Equal(t, p.Insert(2), false, "5")
+	assert.Contains(t, []int{2}, p.GetRandom(), "6")
+}
